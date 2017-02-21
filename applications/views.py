0ee245bd4fbc9c4b2ed493b0e357ea1ba3e44d47
Ashley Felton <ashley@ropable.com>
2017-02-21 12:38:28 +0800

    Create 'assess' task on creation of application object. Added list of tasks to home and application detail views.

--- applications/views.py
+++ applications/views.py
@@ -1,15 +1,18 @@
+from django.http import HttpResponseRedirect
 from django.views.generic import TemplateView, ListView, DetailView, CreateView
-from .models import Application
+from .models import Application, Task
 from .forms import ApplicationForm
 
 
 class HomePage(TemplateView):
+    # TODO: rename this view to something like UserDashboard.
     template_name = 'applications/home_page.html'
 
     def get_context_data(self, **kwargs):
         context = super(HomePage, self).get_context_data(**kwargs)
         context['page_heading'] = 'Home Page'
-        context['page_content'] = 'This is the Statutory Development application.'
+        context['tasks'] = Task.objects.filter(
+            status=Task.TASK_STATUS_CHOICES.ongoing, assignee=self.request.user)
         return context
 
 
@@ -24,3 +27,13 @@ class ApplicationDetail(DetailView):
 class ApplicationCreate(CreateView):
     form_class = ApplicationForm
     template_name = 'applications/application_form.html'
+
+    def form_valid(self, form):
+        """Override form_valid to generate an "Assess" task on the new
+        application, assigned to the request user.
+        """
+        self.object = form.save()
+        Task.objects.create(
+            application=self.object, task_type=Task.TASK_TYPE_CHOICES.assess,
+            status=Task.TASK_STATUS_CHOICES.ongoing, assignee=self.request.user)
+        return HttpResponseRedirect(self.get_success_url())
