e38845c61cb5d0e196591a456aadb20a6d869a2c
Ashley Felton <ashley@ropable.com>
2017-02-21 11:18:46 +0800

    Added crispy_forms to project, example form.

---
+++ applications/forms.py
@@ -0,0 +1,23 @@
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Submit
+from django.forms import ModelForm
+from .models import Application
+
+
+class ApplicationForm(ModelForm):
+
+    class Meta:
+        model = Application
+        fields = ['app_type', 'title', 'description', 'submit_date']
+
+    def __init__(self, *args, **kwargs):
+        super(ApplicationForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.form_id = 'id_form_create_application'
+        self.helper.form_method = 'POST'  # This is default.
+        self.helper.form_action = 'application_create'  # Calls reverse().
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-lg-1 col-md-2'
+        self.helper.field_class = 'col-lg-11 col-md-10'
+        self.helper.add_input(Submit('save', 'Save', css_class='btn-lg'))
+        self.helper.add_input(Submit('cancel', 'Cancel'))
