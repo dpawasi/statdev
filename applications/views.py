727a5740e90d32777121f496edd9230d8a16f23a
Jason <jason.moore@dpaw.wa.gov.au>
2017-03-30 12:30:05 +0800

    Adjusted error wording

--- applications/views.py
+++ applications/views.py
@@ -1100,7 +1100,7 @@ class WebsitePublicationCreate(LoginRequiredMixin, CreateView):
     def get(self, request, *args, **kwargs):
         app = Application.objects.get(pk=self.kwargs['pk'])
         if app.state != app.APP_STATE_CHOICES.draft:
-            messages.errror(self.request, "Can't add new newspaper publication to this application")
+            messages.errror(self.request, "Can't add new Website publication to this application")
             return HttpResponseRedirect(app.get_absolute_url())
         return super(WebsitePublicationCreate, self).get(request, *args, **kwargs)
 
