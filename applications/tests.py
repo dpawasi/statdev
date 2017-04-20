4042f9ea96b9892a1c0c2b083c87dd3a7a4aab37
Brendan Cale <brendan.cale@dpaw.wa.gov.au>
2017-04-20 14:36:04 +0800

    fixed test group

--- applications/tests.py
+++ applications/tests.py
@@ -23,6 +23,7 @@ class ApplicationTest(TestCase):
         self.user2 = mixer.blend(User, email=random_dpaw_email, is_superuser=False, is_staff=True)
         self.user2.set_password('pass')
         self.user2.save()
+        Group.objects.add(name='Emergency')
         processor = Group.objects.get(name='Processor')
         assessor = Group.objects.get(name='Assessor')
         approver = Group.objects.get(name='Approver')
