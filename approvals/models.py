a5acf080b127f4f07fbd85749194263700fbfffa
Jason <jason.moore@dpaw.wa.gov.au>
2017-06-09 12:31:23 +0800

    Approval Migration Updates

--- approvals/models.py
+++ approvals/models.py
@@ -23,13 +23,14 @@ class Approval(models.Model):
      app_type = models.IntegerField(choices=Application.APP_TYPE_CHOICES)
      title = models.CharField(max_length=254)
      applicant = models.ForeignKey(settings.AUTH_USER_MODEL, blank=True, null=True, on_delete=models.PROTECT, related_name='applicant_holder') 
-     application = models.ForeignKey(Application, on_delete=models.CASCADE,null=True, blank=True) 
+     application = models.ForeignKey(Application, on_delete=models.CASCADE,null=True, blank=True, related_name='application') 
      start_date = models.DateField(null=True, blank=True)
      expiry_date = models.DateField(null=True, blank=True)
      status = models.IntegerField(choices=APPROVAL_STATE_CHOICES) 
      approval_document = models.ForeignKey(Record, null=True, blank=True, related_name='approval_document')
      suspend_from_date = models.DateField(null=True, blank=True)
      suspend_to_date = models.DateField(null=True, blank=True)
+     ammendment_application  = models.ForeignKey(Application, on_delete=models.CASCADE,null=True, blank=True, related_name='ammendment_application')
 
 
      def __str__(self):
