a2a22728f8d9c9c2c4551fc61b45708d04b9638b
Jason <jason.moore@dpaw.wa.gov.au>
2017-07-04 11:08:57 +0800

    Updates

--- applications/views.py
+++ applications/views.py
@@ -1207,7 +1207,7 @@ class ApplicationUpdate(LoginRequiredMixin, UpdateView):
 
         if self.object.app_type == self.object.APP_TYPE_CHOICES.licence:
             form.save_m2m()
-        if self.request.POST.get('save'):
+#        if self.request.POST.get('save'):
 #        if self.request.POST.get('nextstep') or self.request.POST.get('prevstep'):
             # print self.request.POST['nextstep']          
             # if self.request.POST.get('prevstep'):
