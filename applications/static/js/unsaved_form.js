afb8694737c6595e990cd33647c9b9f827017655
jason.moore@dbca.wa.gov.au <Jason Moore>
2017-11-28 02:57:42 +0000

    Unsaved Model Box.

--- applications/static/js/unsaved_form.js
+++ applications/static/js/unsaved_form.js
@@ -1,6 +1,7 @@
 var django_form_checks = { 
 	var: {
 	     'form_changed': null,
+             'href': null,
 	},
         load: function() { 
 	      $("form input[type=text]").change(function() { 
@@ -10,14 +11,37 @@ var django_form_checks = {
 
 	      $("a").click(function(e) { 
                          var href= $(this).attr('href');
+                         django_form_checks.var.href= href;
                          var reg = new RegExp(/^#.*$/);
                          if (href.match(reg)) {
 			 } else {
 		         	if (django_form_checks.var.form_changed == 'changed') {
-				      var success = confirm('You have unsaved changes, please click the save button to avoid losing data,  are you sure you want to navigate away from this page?'); 
-				      if (success == false) { 
+		//		      var success = confirm('You have unsaved changes, please click the save button to avoid losing data,  are you sure you want to navigate away from this page?'); 
+
+ $('#myModal').modal({
+        show: 'false'
+    }); 
+return false;
+ $( function() {
+    $( "#myModal" ).dialog({
+      resizable: false,
+      height: "auto",
+      width: 400,
+      modal: true,
+      buttons: {
+        "Yes": function() {
+          window.location = href;
+          $( this ).dialog( "close" );
+        },
+        "No": function() {
+          $( this ).dialog( "close" );
+        }
+      }
+    });
+  } );
+//    			      if (success == false) { 
 					return false;
-				      }
+//				      }
 				}
 
 			}
@@ -26,8 +50,12 @@ var django_form_checks = {
 
 
         },
+        loadUrl: function() {
+	window.location = django_form_checks.var.href;
+
+	}
 };
 
-window.onload = function () {
-	django_form_checks.load();
-}
+//window.onload = function () {
+//	django_form_checks.load();
+//}
